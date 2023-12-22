873d26828094730a464bf9351d44ae14645f1b8f
Ritik Raj <raj769417@gmail.com>
2023-12-22 22:55:09 +0530

    added nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <>
+            <Nav />
+        </>
+    );
 }
 
-export default App;
+function Nav() {
+    return (
+        <nav className="nav flex container">
+            <div className="nav__left brand">🍿 moviePedia</div>
+            <div className="nav__center">
+                <input placeholder="Search movie" />
+            </div>
+            <div className="nav__right">Found 10 results</div>
+        </nav>
+    );
+}
