93bb94da26afbcb460e4eedb30634ce2948bd172
Ritik Raj <raj769417@gmail.com>
2023-12-23 20:17:17 +0530

    added fetch movie-detail logic

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ export default function App() {
     const [movies, setMovies] = useState([]);
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
+    const [selected, setSelected] = useState(null);
 
-    console.log("Render 1");
+    function handleSelectMovie(id) {
+        setSelected((selected) => (id !== selected ? id : null));
+    }
 
     useEffect(
         function () {
@@ -17,38 +20,48 @@ export default function App() {
                     const res = await fetch(
                         "https://omdbapi.com/?apikey=34d0a473&s=" + search
                     );
-                    const result = await res.json();
-
-                    if (result.Response !== "True") {
-                        setMovies([]);
-                        throw Error("😫 Cannot find any movie !!!");
+                    if (!res.ok) {
+                        throw new Error("😐 Something went wrong");
                     }
+                    const data = await res.json();
 
-                    const data = result["Search"];
-                    setMovies(data);
+                    if (data.Response !== "True") {
+                        setSelected(null);
+                        throw new Error("😫 Cannot find any movie !!!");
+                    }
+                    setMovies(data.Search);
                 } catch (e) {
                     setError(e.message);
                 } finally {
                     setLoading(false);
                 }
             }
-            if (search.length < 3) return;
+            if (search.length < 3) {
+                setMovies([]);
+                setError("");
+                setSelected(null);
+                return;
+            }
             fetchMovies();
-            console.log("Render 2");
         },
         [search]
     );
 
     return (
         <>
-            <Nav onChange={setSearch} movies={movies} />
+            <Nav onChange={setSearch} search={search} movies={movies} />
             <div className="container box-container">
                 <Box>
                     {loading && <Loader />}
                     {error && <div className="error">{error}</div>}
-                    {!loading && !error && <MovieList movies={movies} />}
+                    {!loading && !error && (
+                        <MovieList
+                            movies={movies}
+                            onSelectMovie={handleSelectMovie}
+                        />
+                    )}
                 </Box>
-                <Box />
+                <Box>{selected && <MovieDetail selected={selected} />}</Box>
             </div>
         </>
     );
@@ -58,12 +71,13 @@ function Loader() {
     return <div className="loading">Loading...</div>;
 }
 
-function Nav({ onChange, movies }) {
+function Nav({ onChange, movies, search }) {
     return (
         <nav className="nav flex container">
-            <div className="nav__left brand">🍿 moviePedia</div>
+            <div className="nav__left brand">🍿 <span className="brand">moviePedia</span></div>
             <div className="nav__center">
                 <input
+                    value={search}
                     placeholder="Search movie"
                     onChange={(e) => onChange(e.target.value)}
                 />
@@ -77,25 +91,97 @@ function Box({ children }) {
     return <div className="box">{children}</div>;
 }
 
-function MovieList({ movies }) {
+function MovieList({ movies, onSelectMovie }) {
     return (
         <div className="movie-list">
             {movies.map((movie) => (
-                <Movie movie={movie} key={movie.imdbID} />
+                <Movie
+                    movie={movie}
+                    key={movie.imdbID}
+                    onSelectMovie={onSelectMovie}
+                />
             ))}
         </div>
     );
 }
 
-function Movie({ movie }) {
+function Movie({ movie, onSelectMovie }) {
     const { Title, Year, Poster } = movie;
     return (
-        <div className="movie">
+        <div className="movie" onClick={() => onSelectMovie(movie.imdbID)}>
             <img src={`${Poster}`} alt="movie" className="movie__image" />
             <div className="movie__detail">
                 <div className="title">{Title}</div>
                 <span>🗓️ {Year}</span>
             </div>
+        </div>
+    );
+}
+
+function MovieDetail({ selected }) {
+    const [movie, setMovie] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    const {
+        Title: title,
+        Released: released,
+        Runtime: runtime,
+        Genre: genre,
+        imdbRating: rating,
+        Poster: poster,
+    } = movie;
+
+    console.log("movie = ", movie);
+
+    useEffect(
+        function () {
+            async function fetchMovieDetails() {
+                setError("");
+                setLoading(true);
+                try {
+                    const res = await fetch(
+                        "https://omdbapi.com/?apikey=34d0a473&i=" + selected
+                    );
+
+                    if (!res.ok) {
+                        throw new Error("😐 Something went wrong.");
+                    }
+                    const data = await res.json();
+                    setMovie(data);
+                } catch (e) {
+                    setError(e.message);
+                } finally {
+                    setLoading(false);
+                    setError("");
+                }
+            }
+            fetchMovieDetails();
+        },
+        [selected]
+    );
+
+    return (
+        <div className="movie-detail">
+            {loading ? (
+                <Loader />
+            ) : (
+                <div className="movie-detail__head flex">
+                    <img
+                        src={`${poster}`}
+                        className="movie-detail__image"
+                        alt="movie"
+                    />
+                    <div className="movie-detail__right flex">
+                        <h2>{title}</h2>
+                        <span>
+                            {released} &#183; {runtime}
+                        </span>
+                        <span>{genre}</span>
+                        <span>⭐ {rating} IMDb rating </span>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
